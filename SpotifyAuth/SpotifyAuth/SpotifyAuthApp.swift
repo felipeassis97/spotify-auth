fe015477fbcfd75d76318abe06e7bb0229c51bbf
Felipe Assis <felipeassis97@gmail.com>
2024-02-05 08:02:35 -0300

    feat: initial structure project

---
+++ SpotifyAuth/SpotifyAuth/SpotifyAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyAuthApp.swift
+//  SpotifyAuth
+//
+//  Created by Felipe Assis on 05/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
