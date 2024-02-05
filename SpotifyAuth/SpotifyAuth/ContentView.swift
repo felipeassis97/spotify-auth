380d14003336077d692ea2957f076055a3e549f5
Felipe Assis <felipeassis97@gmail.com>
2024-02-05 08:57:34 -0300

    feat: Create assets and fonts

--- SpotifyAuth/SpotifyAuth/ContentView.swift
+++ SpotifyAuth/SpotifyAuth/ContentView.swift
@@ -13,7 +13,9 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
             Text("Hello, world!")
+                .font(.customStyle(style: .bold, size: 25))
         }
         .padding()
     }
