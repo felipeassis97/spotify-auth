ef8333c9e61747b3ede77d0aa4878f444e7f5cb5
Felipe Assis <felipeassis97@gmail.com>
2024-02-15 09:12:12 -0300

    feat: edit profile view

--- SpotifyAuth/Common/Components/Buttons/PrimaryButtonStyle.swift
+++ SpotifyAuth/Common/Components/Buttons/PrimaryButtonStyle.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
+    var backgroundColor: Color = .primaryGreen
+    var foregroundColor: Color = .black
+    var fontSize: CGFloat = 16
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .font(.customStyle(style: .bold, size: 16))
+            .font(.customStyle(style: .bold, size: fontSize))
             .padding()
-            .background(.primaryGreen)
-            .foregroundColor(.black)
+            .background(backgroundColor)
+            .foregroundColor(foregroundColor)
             .clipShape(RoundedRectangle(cornerRadius: 50))
             .opacity(configuration.isPressed ? 0.5 : 1)
     }
--- SpotifyAuth/Features/Library/LibraryView.swift
+++ SpotifyAuth/Features/Library/LibraryView.swift
@@ -13,69 +13,97 @@ struct LibraryView: View {
     @State private var selectedPhoto: PhotosPickerItem?
     
     var body: some View {
-        VStack {
+        VStack(spacing: 24) {
             ProfileImageView()
+            Button {
+                print("")
+            } label: {
+                Text("Edit profile")
+                    .font(.customStyle(style: .bold, size: 16))
+            }
+            .buttonStyle(PrimaryButtonStyle(backgroundColor: .gray.opacity(0.8),
+                                            foregroundColor: .white,
+                                            fontSize: 12))
             Spacer()
-            Text(authViewModel.currentUser?.fullName ?? "")
-                .font(.customStyle(style: .bold, size: 16))
         }
         
     }
 }
 
 
-
 struct ProfileImageView: View {
     @Environment(AuthViewModel.self) var authViewModel
-    @State private var selectedPhoto: PhotosPickerItem?
-    
     var body: some View {
-        VStack {
-            
-            ZStack {
-                if authViewModel.currentUser?.profileImage != nil {
-                    if let profile = UIImage(data: authViewModel.currentUser!.profileImage!)  {
-                        Image(uiImage: profile)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(maxWidth: 100)
-                            .clipShape(Circle())
-                            .padding(.bottom, 16)
-                    }
-                } else {
-                    Circle()
-                        .foregroundStyle(.gray)
-                        .scaledToFit()
-                        .frame(width: 100)
-                        .padding(.bottom, 16)
-                }
-                
-                PhotosPicker(selection: $selectedPhoto, matching: .images) {
-                    Image(systemName: "pencil")
-                }
-                .onChange(of: selectedPhoto)  {
-                    if selectedPhoto != nil {
-                        Task {
-                            authViewModel.currentUser?.profileImage = try await selectedPhoto?.loadTransferable(type: Data.self)
-                        }
-                    }
-                }
+        if authViewModel.currentUser?.profileImage != nil {
+            if let profile = UIImage(data: authViewModel.currentUser!.profileImage!)  {
+                Image(uiImage: profile)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(maxWidth: 100)
+                    .clipShape(Circle())
             }
-            
-            if selectedPhoto != nil {
-                Button {
-                    Task {
-                        await authViewModel.saveToStorage(pickerImage: selectedPhoto!)
-                        selectedPhoto = nil
-                    }
-                } label: {
-                    Text("Save changes")
-                }
-                .buttonStyle(PrimaryButtonStyle())
-            }
+        } else {
+            Circle()
+                .scaledToFit()
+                .frame(maxWidth: 100)
+                .foregroundStyle(.gray.opacity(0.8))
+
         }
     }
 }
+
+
+//struct ProfileImageView: View {
+//    @Environment(AuthViewModel.self) var authViewModel
+//    @State private var selectedPhoto: PhotosPickerItem?
+//
+//    var body: some View {
+//        VStack {
+//
+//            ZStack {
+//                if authViewModel.currentUser?.profileImage != nil {
+//                    if let profile = UIImage(data: authViewModel.currentUser!.profileImage!)  {
+//                        Image(uiImage: profile)
+//                            .resizable()
+//                            .scaledToFit()
+//                            .frame(maxWidth: 100)
+//                            .clipShape(Circle())
+//                            .padding(.bottom, 16)
+//                    }
+//                } else {
+//                    Circle()
+//                        .foregroundStyle(.gray)
+//                        .scaledToFit()
+//                        .frame(width: 100)
+//                        .padding(.bottom, 16)
+//                }
+//
+//                PhotosPicker(selection: $selectedPhoto, matching: .images) {
+//                    Image(systemName: "pencil")
+//                }
+//                .onChange(of: selectedPhoto)  {
+//                    if selectedPhoto != nil {
+//                        Task {
+//                            authViewModel.currentUser?.profileImage = try await selectedPhoto?.loadTransferable(type: Data.self)
+//                        }
+//                    }
+//                }
+//            }
+//
+//            if selectedPhoto != nil {
+//                Button {
+//                    Task {
+//                        await authViewModel.saveToStorage(pickerImage: selectedPhoto!)
+//                        selectedPhoto = nil
+//                    }
+//                } label: {
+//                    Text("Save changes")
+//                }
+//                .buttonStyle(PrimaryButtonStyle())
+//            }
+//        }
+//    }
+//}
 
 #Preview {
     LibraryView()
